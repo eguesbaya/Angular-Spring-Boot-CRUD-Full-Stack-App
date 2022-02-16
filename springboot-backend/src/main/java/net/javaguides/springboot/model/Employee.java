43733fd346c6214acbdaec56f8d4e17f81a24b6b
Ems <guesbaya@hotmail.fr>
2022-02-16 16:35:13 +0100

    first commit

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,41 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Employee(String firstname, String lastname, String emailId) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.emailId = emailId;
+    }
+
+}
