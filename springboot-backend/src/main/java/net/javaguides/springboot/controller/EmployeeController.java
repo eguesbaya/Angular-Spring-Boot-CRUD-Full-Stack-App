7563898fa9d095b857f307284bea057c85f2ad7c
eguesbaya <guesbaya@hotmail.fr>
2022-02-24 17:27:01 +0100

    created update api

--- springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,12 +3,16 @@ package net.javaguides.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import net.javaguides.springboot.exception.ResourceNotFoundException;
 
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
@@ -31,6 +35,32 @@ public class EmployeeController {
     @PostMapping(path = "/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with id " + id + " does not exist."));
+        return ResponseEntity.ok(employee);
+    }
+
+    //update employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee input) {
+        Employee employee = employeeRepository.findById(id).get();
+        if (input.getFirstName() != null) {
+            employee.setFirstName(input.getFirstName());
+        }
+        if (input.getLastName() != null) {
+           employee.setLastName(input.getLastName()); 
+        }
+        if (input.getEmailId() != null) {
+            employee.setEmailId(input.getEmailId());
+        }
+        
+
+        return ResponseEntity.ok(employeeRepository.save(employee));
     }
 
 }
--- springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/exception/ResourceNotFoundException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFound extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
-    public ResourceNotFound(String message) {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
